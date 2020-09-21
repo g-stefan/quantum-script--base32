3a90b0b2d62194d475ee724318fadf7678d00f7a
Grigore Stefan <g_stefan@yahoo.com>
2020-09-22 01:34:25 +0300

    update

--- port/build.ubuntu.make.sh
+++ port/build.ubuntu.make.sh
@@ -17,5 +17,5 @@ cmdX(){
 	fi
 }
 
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-base32.static.compile.info
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-base32.dynamic.compile.info
+cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-base32.static.compile
+cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-base32.dynamic.compile
